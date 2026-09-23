bade1e75c2b4bfd969ff77fc9f86d8c3f1db4253
thaiyaa <thainak24406h@st.uel.edu.vn>
2026-09-23 23:07:30 +0700

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { ServiceProductImageEventComponent } from './components/service-product-image-event/service-product-image-event.component';
+import { ServiceProductImageEventDetailComponent } from './components/service-product-image-event-detail/service-product-image-event-detail.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'service-product-image-event', pathMatch: 'full' },
+  // Bài 13
+  { path: 'service-product-image-event', component: ServiceProductImageEventComponent },
+  { path: 'service-product-image-event/:id', component: ServiceProductImageEventDetailComponent },
+  // Bài 14
+  { path: 'catalog', component: CatalogComponent },
+];
